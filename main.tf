05a2f518451e743dabd78f7480b18a06d4d3c6e9
Nicole Ann Hargrove <nicoleannhargrove@gmail.com>
2021-06-18 20:23:32 +0000

    Add remo backend and variable definitions

--- main.tf
+++ main.tf
@@ -69,3 +69,9 @@ resource "aws_s3_bucket_object" "app" {
 output "Endpoint" {
   value = aws_s3_bucket.app.website_endpoint
 }
+
+backend "s3" {
+  bucket = "circle-ci-backend-20210618201906813100000001"
+  key = "terraform/webapp/terraform.tfstate"
+  region = "us-east=1"
+}
